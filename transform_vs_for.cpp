9ec5739c5cc8fc64e824fca8ce68238baab47548
Mehmet Cagri Aksoy <mcagriaksoy@yandex.com>
2023-10-31 12:08:14 +0100

    std::Transform comparison add
    Signed-off-by: Mehmet Cagri Aksoy <mcagriaksoy@yandex.com>

---
+++ transform_vs_for.cpp
@@ -0,0 +1,63 @@
+// Creator: Mehmet Cagri Aksoy
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define NUMBER_OF_TESTS 100
+#define SIZE 1000000
+
+// Range based loop vs std:transform loop single core performance test
+int main()
+{
+    vector<int> v(SIZE);
+
+    clock_t start;
+    clock_t end;
+    double time_taken = 0;
+    int test_number = NUMBER_OF_TESTS;
+
+    // TEST 1
+    while (test_number--)
+    {
+        start = clock();
+        transform(v.begin(), v.end(), v.begin(), [](int i)
+                  { return i + 1; });
+        end = clock();
+        time_taken += (double(end - start) / double(CLOCKS_PER_SEC));
+    }
+    time_taken /= NUMBER_OF_TESTS;
+    cout << "Average time consumption \"std:transform\" loop: " << time_taken << " seconds" << endl;
+
+    // TEST 2
+    vector<int> v2(SIZE);
+    double time_taken2 = 0;
+    test_number = NUMBER_OF_TESTS;
+    while (test_number--)
+    {
+        start = clock();
+        for (auto &e : v2)
+        {
+            ++e;
+        }
+        end = clock();
+        time_taken2 += double(end - start) / double(CLOCKS_PER_SEC);
+    }
+    time_taken2 /= NUMBER_OF_TESTS;
+    cout << "Average time consumption \"Range based loop\" loop: " << time_taken2 << " seconds" << endl;
+    // Comparison
+    if (time_taken2 > time_taken)
+    {
+        cout << "std:transform loop is " << time_taken2 / time_taken << " times faster than Range based loop" << endl;
+        cout << "std:transform loop is " << time_taken2 - time_taken << " seconds faster than Range based loop" << endl;
+        cout << "Performance difference percentage: " << (time_taken2 - time_taken) / time_taken2 * 100 << "%" << endl;
+    }
+    else
+    {
+        cout << "Range based loop loop is " << time_taken / time_taken2 << " times faster than std:transform" << endl;
+        cout << "Range based loop loop is " << time_taken - time_taken2 << " seconds faster than std:transform" << endl;
+        cout << "Performance difference percentage: " << (time_taken - time_taken2) / time_taken * 100 << "%" << endl;
+    }
+}
